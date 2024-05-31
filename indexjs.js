0d447fcba43f1e0116a0d2ce24f241552f72cff7
felipedue80 <felipedue80+1@gmail.com>
2024-05-31 18:02:44 -0500

    creacion menu didactico

--- indexjs.js
+++ indexjs.js
@@ -13,3 +13,19 @@ window.addEventListener('resize', function() {
     }
 });
 window.dispatchEvent(new Event('resize')); // Ejecuta el evento resize al cargar la página
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+    var dropbtn = document.querySelector(".dropbtn");
+    var dropdownContent = document.querySelector(".dropdown-content");
+    dropbtn.addEventListener("click", function(){
+      dropdownContent.classList.toggle("show");
+    });
+
+    var dropbtn2 = document.querySelector(".dropbtn2");
+    var dropdownContent2 = document.querySelector(".dropdown-content2");
+    dropbtn2.addEventListener("click", function(){
+      dropdownContent2.classList.toggle("show");
+    });
+
+  });
